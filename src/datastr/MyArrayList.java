5a20f39511f10d2f34ce7e812938686f6b04d311
Igors Šaters <s21sateigor@venta.lv>
2023-02-27 09:41:07 +0200

    Edit: Added new elements

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,4 +1,31 @@
 package datastr;
 
-public class MyArrayList {
+public class MyArrayList
+{
+    private char[] elements;
+    private final int DEFAULT_ARRAY_SIZE = 6;
+    private int arraySize = DEFAULT_ARRAY_SIZE;
+    private int elementCounter = 0;
+
+
+
+    public MyArrayList(){
+        elements = new char[arraySize]; //array with 6 cells
+    }
+
+    public MyArrayList(int inputArraySize){
+        if(inputArraySize > 0){
+            arraySize = inputArraySize;
+        }
+        elements = new char[arraySize];
+    }
+
+    public void isFull(){
+
+    }
+
+    public void isEmpty(){
+
+    }
 }
+
